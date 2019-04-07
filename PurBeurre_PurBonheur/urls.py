bc4a19f782476529ab59036843fd7307820c4950
SebastienLesouef <sebastienlesouef@gmail.com>
2019-04-07 15:59:16 +0200

    hello world

--- PurBeurre_PurBonheur/urls.py
+++ PurBeurre_PurBonheur/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 
+from Substitute import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index)
+    # path('admin/', admin.site.urls),
 ]

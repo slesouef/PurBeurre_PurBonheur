65c322a2efbdbab24019654bea9cf614b2afceeb
SebastienLesouef <sebastienlesouef@gmail.com>
2019-04-07 16:49:27 +0200

    app name modified

--- PurBeurre_PurBonheur/settings.py
+++ PurBeurre_PurBonheur/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Substitute.apps.SubstituteConfig',
+    'search.apps.SubstituteConfig',
 ]
 
 MIDDLEWARE = [
--- PurBeurre_PurBonheur/urls.py
+++ PurBeurre_PurBonheur/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 
-from Substitute import views
+from search import views
 
 urlpatterns = [
     path('', views.index)

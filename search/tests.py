b1eb822f52cb71e53bf3cc99808fe7fa0665f596
SebastienLesouef <sebastienlesouef@gmail.com>
2019-04-19 11:03:52 +0200

    tests for index and legal OK response

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# index page
+class IndexPageTestCase(TestCase):
+    # test that index page returns http 200
+    def test_index_page(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+
+# legal mention page
+class LegalPageTestCase(TestCase):
+    # test that legal page returns http 200
+    def test_legal_page(self):
+        response = self.client.get('/legal/')
+        self.assertEqual(response.status_code, 200)

73c6a4ecc96b89a52a0c32ab143859017d163038
SebastienLesouef <sebastienlesouef@gmail.com>
2019-04-24 10:50:43 +0200

    Configuration module

---
+++ search/conf.py
@@ -0,0 +1,6 @@
+"""Configuration file for the project"""
+
+# Open Food Facts API call URL
+URL = 'https://fr.openfoodfacts.org/cgi/search.pl?action=process&'\
+      'sort_by=unique_scans_n&json=1&search_terms='
+PAGE_SIZE = 20

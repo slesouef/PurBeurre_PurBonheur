c5d788f05215ca695baa8b8e9cdf18b038e032d3
SebastienLesouef <sebastienlesouef@gmail.com>
2019-05-12 17:29:17 +0200

    login page test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class LoginPagTestCase(TestCase):
+
+    def test_login_page(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)

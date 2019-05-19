56850273d0484b183fa733f3559c051c28da5aeb
SebastienLesouef <sebastienlesouef@gmail.com>
2019-05-19 14:53:59 +0200

    user model extends AbstractUser instead of AbstractBaseUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 
-class MyManager(BaseUserManager):
-    def _create_user(self, email, password, **extra_fields):
+class MyUser(AbstractUser):
+
+    avatar = models.ImageField(blank=True)
+
+    REQUIRED_FIELDS = ['first_name', 'email']
+
+    def get_full_name(self):
         """
-        Create and save a user with the given email and password.
+        Returns the first_name plus the last_name, with a space in between.
         """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
 
-    def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, **extra_fields)
-
-
-class MyUser(AbstractBaseUser):
-    object = MyManager()
-
-    first_name = models.CharField(max_length=254)
-    last_name = models.CharField(max_length=254)
-    email = models.EmailField(unique=True)
-    avatar = models.ImageField()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    def get_short_name(self):
+        """
+        Returns the short name for the user.
+        """
+        return self.first_name

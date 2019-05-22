cd73efa63a24f1871a76205165f3040f8da37f50
SebastienLesouef <sebastienlesouef@gmail.com>
2019-05-22 11:08:37 +0200

    logged in users cannot access signup page

--- accounts/views.py
+++ accounts/views.py
@@ -6,25 +6,28 @@ from .forms import SignUpForm
 
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            # TODO: user picture save at signup
-            user = form.save(commit=False)
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user.set_password(raw_password)
-            user.save()
-            authenticated_user = authenticate(request, username=username,
-                                              password=raw_password)
-            if authenticated_user is not None:
-                login(request, authenticated_user)
-                return redirect('profile')
-            else:
-                user.delete()
+    if request.user.is_authenticated:
+        return redirect('profile')
     else:
-        form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                # TODO: user picture save at signup
+                user = form.save(commit=False)
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password')
+                user.set_password(raw_password)
+                user.save()
+                authenticated_user = authenticate(request, username=username,
+                                                  password=raw_password)
+                if authenticated_user is not None:
+                    login(request, authenticated_user)
+                    return redirect('profile')
+                else:
+                    user.delete()
+        else:
+            form = SignUpForm()
+        return render(request, 'accounts/signup.html', {'form': form})
 
 
 @login_required()

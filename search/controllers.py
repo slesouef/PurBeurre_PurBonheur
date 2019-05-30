73957296eb275f1fc2ee403303c729ce29a76464
SebastienLesouef <sebastienlesouef@gmail.com>
2019-05-30 11:51:03 +0200

    nutritional values added to insert in database

--- search/controllers.py
+++ search/controllers.py
@@ -72,8 +72,61 @@ def save_data(data):
                      rating=rating, url=url, category=cat)
         if "image_url" in item:
             image = item["image_url"]
-            p.image =image
+            p.image = image
         p.save()
+        v = NutritionalValues()
+        v.pid = p
+        if "serving_size" in item:
+            v.serving_size = item["serving_size"]
+        if "energy_serving" in item["nutriments"]:
+            v.energy = item["nutriments"]["energy_serving"]
+        if "energy_100g" in item["nutriments"]:
+            v.energy_per100 = item["nutriments"]["energy_100g"]
+        if "energy_unit" in item["nutriments"]:
+            v.energy_unit = item["nutriments"]["energy_unit"]
+        if "fat_serving" in item["nutriments"]:
+            v.fat = item["nutriments"]["fat_serving"]
+        if "fat_100g" in item["nutriments"]:
+            v.fat_per100 = item["nutriments"]["fat_100g"]
+        if "fat_unit" in item["nutriments"]:
+            v.fat_unit = item["nutriments"]["fat_unit"]
+        if "saturated-fat_serving" in item["nutriments"]:
+            v.saturatedFat = item["nutriments"]["saturated-fat_serving"]
+        if "saturated-fat_100g" in item["nutriments"]:
+            v.saturatedFat_per100 = item["nutriments"]["saturated-fat_100g"]
+        if "saturated-fat_unit" in item["nutriments"]:
+            v.saturatedFat_unit = item["nutriments"]["saturated-fat_unit"]
+        if "cabohydrates_serving" in item["nutriments"]:
+            v.carbohydrates = item["nutriments"]["carbohydrates_serving"]
+        if "carbohydrates_100g" in item["nutriments"]:
+            v.carbohydrates_per100 = item["nutriments"]["carbohydrates_100g"]
+        if "carbohydrates_unit" in item["nutriments"]:
+            v.carbohydrates_unit = item["nutriments"]["carbohydrates_unit"]
+        if "sugars_serving" in item["nutriments"]:
+            v.sugar = item["nutriments"]["sugars_serving"]
+        if "sugars_100g" in item["nutriments"]:
+            v.sugar_per100 = item["nutriments"]["sugars_100g"]
+        if "sugars_unit" in item["nutriments"]:
+            v.sugar_unit = item["nutriments"]["sugars_unit"]
+        if "fiber_serving" in item["nutriments"]:
+            v.fiber = item["nutriments"]["fiber_serving"]
+        if "fiber_100g" in item["nutriments"]:
+            v.fiber_per100 = item["nutriments"]["fiber_100g"]
+        if "fiber_unit" in item["nutriments"]:
+            v.fiber_unit = item["nutriments"]["fiber_unit"]
+        if "proteins_serving" in item["nutriments"]:
+            v.proteins = item["nutriments"]["proteins_serving"]
+        if "proteins_100g" in item["nutriments"]:
+            v.proteins_per100 = item["nutriments"]["proteins_100g"]
+        if "proteins_unit" in item["nutriments"]:
+            v.proteins_unit = item["nutriments"]["proteins_unit"]
+        if "salt_serving" in item["nutriments"]:
+            v.salt = item["nutriments"]["salt_serving"]
+        if "salt_100g" in item["nutriments"]:
+            v.salt_per100 = item["nutriments"]["salt_100g"]
+        if "salt_unit" in item["nutriments"]:
+            v.salt_unit = item["nutriments"]["salt_unit"]
+        v.save()
 
 
 def populate_database(query):

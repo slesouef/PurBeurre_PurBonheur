00b991380ba4ef4d7eb5f6d422bb71a94c2c75be
SebastienLesouef <sebastienlesouef@gmail.com>
2019-05-30 19:48:10 +0200

    search result in context

--- search/views.py
+++ search/views.py
@@ -4,27 +4,27 @@ from .controllers import populate_database
 from .search_products import get_suggestions
 
 
-
 def index(request):
-    return render(request, 'search/index.html')
+    return render(request, "search/index.html")
 
 
 def legal(request):
-    return render(request, 'search/legal.html')
+    return render(request, "search/legal.html")
 
 
 def search(request):
-    query = request.POST.get('query')
-    context ={}
-    context['title'] = "Resultat de la recherche {}".format(query)
+    query = request.POST.get("query")
+    context = {"title": "Resultat de la recherche {}".format(query)}
     try:
-        products = get_suggestions(query)
-        context['products'] = products
+        result, products = get_suggestions(query)
+        context["result"] = result
+        context["products"] = products
     except LookupError:
         try:
             populate_database(query)
-            products = get_suggestions(query)
-            context['products'] = products
+            result, products = get_suggestions(query)
+            context["result"] = result
+            context["products"] = products
         except ValueError:
-            context['error'] = "Votre recherche n'a donne aucun resultats"
-    return render(request, 'search/results.html', context)
+            context["error"] = "Votre recherche n'a donne aucun resultats"
+    return render(request, "search/results.html", context)

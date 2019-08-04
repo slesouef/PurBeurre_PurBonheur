aca0fde8c9556700118cbbb285d1a49ae57fc841
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-04 18:16:38 +0200

    user file upload to user specific directory

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import AbstractUser
 from search.models import Products
 
 
+def user_directory_path(instance, filename):
+    # the user avatar image will be uploaded to MEDIA_ROOT/<username>/<filename>
+    return f'{instance.username}/{filename}'
+
+
 class MyUser(AbstractUser):
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to=user_directory_path)
     favorites = models.ManyToManyField(Products,
                                        related_name='favorites')
 
@@ -16,7 +21,7 @@ class MyUser(AbstractUser):
         """
         Returns the first_name plus the last_name, with a space in between.
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = f'{self.first_name} {self.last_name}'
         return full_name.strip()
 
     def get_short_name(self):

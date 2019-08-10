b2b1d3ddec7aac740bdc69bd17b0a12dd7228483
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-10 16:20:05 +0200

    typo in save_favorite url

--- search/static/search/js/javascript.js
+++ search/static/search/js/javascript.js
@@ -2,7 +2,7 @@ function save_favorite(entry) {
     event.preventDefault();
     let form = new FormData(entry);
     let req = new XMLHttpRequest();
-    req.open("POST", "/accounts/favorite/new/");
+    req.open("POST", "/accounts/favorites/new/");
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
             console.log(req.responseText)

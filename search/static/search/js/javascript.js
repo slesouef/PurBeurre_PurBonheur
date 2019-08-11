2dba1de21f67e530675ce7c029a11f65c22b1355
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-11 17:29:03 +0200

    saveFavorite behaviour implemented

--- search/static/search/js/javascript.js
+++ search/static/search/js/javascript.js
@@ -1,11 +1,12 @@
-function save_favorite(entry) {
+function saveFavorite(entry) {
     event.preventDefault();
     let form = new FormData(entry);
     let req = new XMLHttpRequest();
+    let id = entry.getAttribute("id");
     req.open("POST", "/accounts/favorites/new/");
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            console.log(req.responseText);
+            checkResponse(req.responseText, id);
         } else if (req.status === 403) {
             document.getElementsByTagName('html')[0].innerHTML = req.responseText;
         } else {
@@ -16,4 +17,20 @@ function save_favorite(entry) {
         console.error("Network error");
     });
     req.send(form);
+}
+
+function checkResponse(body, id) {
+    let response = JSON.parse(body);
+    let button_id = "save-" + id;
+    if (response.error === "favorite already exists") {
+        document.getElementById(button_id).className = "btn btn-outline-info";
+        document.getElementById(button_id).innerText = "Sauvegarde";
+        let newMessage = document.createElement("div");
+        newMessage.className = "text-info";
+        newMessage.innerText = "Ce produit est deja sauvegarder";
+        document.getElementById(id).appendChild(newMessage);
+    } else {
+        document.getElementById(button_id).className = "btn btn-outline-success";
+        document.getElementById(button_id).innerText = "Sauvegarde";
+    }
 }

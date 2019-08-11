cbbd2e2de2c4bdeb49f4159e706ea33f724bc2f4
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-11 16:34:42 +0200

    403 displayed when saving favorite unauthenticated

--- search/static/search/js/javascript.js
+++ search/static/search/js/javascript.js
@@ -5,15 +5,15 @@ function save_favorite(entry) {
     req.open("POST", "/accounts/favorites/new/");
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            console.log(req.responseText)
+            console.log(req.responseText);
+        } else if (req.status === 403) {
+            document.getElementsByTagName('html')[0].innerHTML = req.responseText;
         } else {
             console.error(req.status + " " + req.statusText);
-            // displayFatalErrorMessage();
         }
     });
     req.addEventListener("error", function () {
         console.error("Network error");
-        // displayFatalErrorMessage();
     });
     req.send(form);
 }

044ad0d9d5fdefbca26444a12c152d6afb315e1e
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-12 17:48:47 +0200

    Products model method test

---
+++ tests/search/tests_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from search.models import Products
+
+
+class ProductModelTestCase(TestCase):
+    """Test the get_absolute_url method from the Products model"""
+
+    def test_get_absolute_url(self):
+        """verify that the url returned by the method contains the product id"""
+        product = Products()
+        product.id = 10
+        url = product.get_absolute_url()
+        self.assertIn('10', url)

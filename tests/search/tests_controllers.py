f0f6300d2d5e94f2784c58cabb5be02fed96d844
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-12 11:23:29 +0200

    cleanup response test refactor

--- tests/search/tests_controllers.py
+++ tests/search/tests_controllers.py
@@ -11,7 +11,10 @@ MOCK_RESULT = {"products": [
         "quantity": "quantity",
         "nutrition_grade_fr": "e",
         "url": "url",
-    },
+    }
+]}
+
+INVALID_RESULTS = {"products": [
 {
         "product_name_fr": "name",
         "brands": "brand",
@@ -56,26 +59,31 @@ MOCK_RESULT = {"products": [
     }
 ]}
 
-INVALID_RESULTS = {"products": []}
+EMPTY_RESULTS = {"products": []}
 
 
-@mock.patch('search.controllers.openfoodfacts')
+@ mock.patch('search.controllers.openfoodfacts')
 class TestControllers(TestCase):
     """Testing the search controllers module behaviour"""
 
     def test_call_api(self, mock_api):
-        """Testing call api method"""
-        mock_api.products.advanced_search.return_value = MOCK_RESULT
+        """Verify call API method returns the raw API results"""
+        mock_api.products.advanced_search.return_value = INVALID_RESULTS
         results = controllers.call_api("query")
-        self.assertEqual(len(results["products"]), 7)
+        self.assertEqual(len(results["products"]), 6)
 
-    def test_cleanup_response(self, mock_api):
-        """Verify that all invalid results are discarded"""
+    def test_cleanup_response_valid_entry(self, mock_api):
+        """Verify that all valid results are returned"""
         results = controllers.cleanup_response(MOCK_RESULT)
         self.assertEqual(len(results), 1)
 
+    def test_cleanup_response_invalid_entries(self, mock_api):
+        """Verify that all invalid results are discarded"""
+        results = controllers.cleanup_response(INVALID_RESULTS)
+        self.assertEqual(len(results), 0)
+
     def test_populate_database_no_results(self, mock_api):
         """Verify that error is raised if the API does not return valid results"""
-        mock_api.products.advanced_search.return_value = INVALID_RESULTS
+        mock_api.products.advanced_search.return_value = EMPTY_RESULTS
         with self.assertRaises(ValueError):
             controllers.populate_database("query")

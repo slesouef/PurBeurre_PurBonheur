22d42c134369cd0229a964dafabffba57f20de2e
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-13 17:02:38 +0200

    S3 bucket config on PROD for user avatar files

--- PurBeurre_PurBonheur/settings.py
+++ PurBeurre_PurBonheur/settings.py
@@ -156,3 +156,17 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'purbeurre-assets'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+
+    AWS_LOCATION = 'media'

e2393e521a3acb2227d167bf12f307dd8fd68c20
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-14 14:48:26 +0200

    models method test

---
+++ tests/accounts/tests_models.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from accounts.models import MyUser, user_directory_path
+
+
+class UserFileUploadPathTestCase(TestCase):
+    """Verify that the file upload path method returns the correct path values"""
+
+    def test_upload_path(self):
+        """The user's avatar file is uploaded in a user specific directory of the user's username"""
+        user = MyUser.objects.create(username='test')
+        path = user_directory_path(user, 'test_file.txt')
+        self.assertIn('test', path)
+        self.assertIn('test_file.txt', path)

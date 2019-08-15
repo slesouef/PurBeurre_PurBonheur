b0b8d8f643989a872bec94dc8da475cbff6a8626
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-15 16:13:20 +0200

    modified test to avoid save in database

--- tests/accounts/tests_models.py
+++ tests/accounts/tests_models.py
@@ -4,11 +4,13 @@ from accounts.models import MyUser, user_directory_path
 
 
 class UserFileUploadPathTestCase(TestCase):
-    """Verify that the file upload path method returns the correct path values"""
+    """Verify that the file upload path method returns the correct path
+     values"""
 
     def test_upload_path(self):
-        """The user's avatar file is uploaded in a user specific directory of the user's username"""
-        user = MyUser.objects.create(username='test')
+        """The user's avatar file is uploaded in a user specific directory of
+        the user's username"""
+        user = MyUser(username='test')
         path = user_directory_path(user, 'test_file.txt')
         self.assertIn('test', path)
         self.assertIn('test_file.txt', path)

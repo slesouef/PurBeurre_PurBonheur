23ba38bf6f1e76952040fd7b60aa0b0322db0185
Sebastien Lesouef <sebastienlesouef@gmail.com>
2019-08-15 16:36:14 +0200

    typo in check_contains tests name

--- tests/search/tests_search_products.py
+++ tests/search/tests_search_products.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from unittest.mock import patch
 
 from search import search_products
-from search.models import Products
+from search.models import Products, Categories
 
 
 class CheckNameTestCases(TestCase):
@@ -26,6 +26,32 @@ class CheckNameTestCases(TestCase):
         results = search_products.check_name('test')
         self.assertEqual(len(results), 1)
         product2 = Products(name='test')
+        mock_db.return_value = [product1, product2]
+        results = search_products.check_name('test')
+        self.assertEqual(len(results), 2)
+
+
+class CheckContainsTestCases(TestCase):
+    """Verify that the check_contains method returns a query object with the
+    products in database whose name are a possible match to the query"""
+
+    @patch('search.models.Products.objects.filter')
+    def test_check_contains_no_hits(self, mock_db):
+        """Verify that the check_contains method returns an empty query set
+        if no matches are found"""
+        empty_set = Products.objects.none()
+        mock_db.return_value = empty_set
+        results = search_products.check_name('test')
+        self.assertEqual(len(results), 0)
+
+    @patch('search.models.Products.objects.filter')
+    def test_check_contains_with_hit(self, mock_db):
+        """Verify that the method returns the query set if a hit is found"""
+        product1 = Products(name='test')
+        mock_db.return_value = [product1]
+        results = search_products.check_name('test')
+        self.assertEqual(len(results), 1)
+        product2 = Products(name='tes')
         mock_db.return_value = [product1, product2]
         results = search_products.check_name('test')
         self.assertEqual(len(results), 2)

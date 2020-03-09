178dee2dac52f92c3dbc2d77787953e721a1dfb4
Sebastien Lesouef <sebastienlesouef@gmail.com>
2020-03-09 21:53:00 +0100

    settings for prod environment

--- PurBeurre_PurBonheur/settings.py
+++ PurBeurre_PurBonheur/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,15 +23,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
                             'g#3k3b!aopvv*)kv^nwx53fb=fy(^k366bwl)s7x!xh^)py4ap')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = False
 
-if os.environ.get('ENV') == 'PRODUCTION':
-    ALLOWED_HOSTS = ['purbeurre-search.herokuapp.com']
-else:
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.159.66.3', 'localhost']
+
 
 # Application definition
 
@@ -83,20 +77,15 @@ WSGI_APPLICATION = 'PurBeurre_PurBonheur.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if os.environ.get('ENV') == 'PRODUCTION':
-    DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'offproducts',
-            'USER': 'sebastien',
-            'PASSWORD': 'sebastien',
-            'HOST': 'localhost',
-            'PORT': '5432',
-            'TEST': {
-                'NAME': 'mytestdatabase',
-            },
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'offproducts',
+        'USER': 'pur_user',
+        'PASSWORD': 'xe)s5xA2]u5',
+        'HOST': 'localhost',
+        'PORT': '',
         },
     }
 
@@ -138,31 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-
-if os.environ.get('ENV') == 'PRODUCTION':
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    STATICFILES_DIR = (
-        os.path.join(PROJECT_ROOT, 'static')
-    )
-
-    STATICFILE_STORAGE = \
-        'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (user avatars)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'purbeurre-assets'
-
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'

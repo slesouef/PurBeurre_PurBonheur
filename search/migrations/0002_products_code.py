975b3353caaaa901a6ed4352fce6552c2f8ff492
Sebastien Lesouef <sebastienlesouef@gmail.com>
2020-07-14 18:38:23 +0200

    migration: DBUpdate algorithm ok - code field type change to str

--- search/migrations/0002_products_code.py
+++ search/migrations/0002_products_code.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='products',
             name='code',
-            field=models.BigIntegerField(null=True),
+            field=models.CharField(max_length=20, null=True),
         ),
     ]
